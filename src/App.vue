d513e801e8ad3beb738d88345da409e5adc7cf4f
Winner <winnpurba@gmail.com>
2021-11-07 22:12:08 +0700

    Create home page

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/build-a-bot-logo.png" />
+          Build-a-bot!
+        </li>
+      </ul>
+    </nav>
+  </header>
   <main>
     <HomePage />
   </main>
@@ -34,5 +44,25 @@ main {
   background-color: white;
   width: 1024px;
   min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
